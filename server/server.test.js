ef8dd143c970bc279fbe1ecbfde1d2e2820999d6
dcphillips99 <dcphillips@wisc.edu>
2021-11-05 19:03:42 -0500

    Added second test for logging in users

--- server/server.test.js
+++ server/server.test.js
@@ -2,17 +2,36 @@ import app from './server'
 const supertest = require('supertest')
 const request = supertest(app) 
 
-test('Check that user is being created', async() => {
-    const user = {
-        username: "tests",
-        password: "testing"
-    };
-  
-    try {
-        const response = await request(app).post('/api/register').send(user)
-        expect(response.statusCode).toBe(200)
-    }  catch (err) {
+    
+    test('Check that user is being created', async() => {
+        const user = {
+            username: "tests",
+            password: "testing"
+        };
+      
+        try {
+            const response = await request(app).post('/api/register').send(user)
+            expect(response.statusCode).toBe(200)
+        }  catch (err) {
+    
+            console.log('Error')
+        }
+      });
 
-        console.log('Error')
-    }
-  });
+
+
+    
+    test('Check that user can login', async() => {
+        const testUser = {
+            username: "test",
+            password: "test"
+        };
+      
+        try {
+            const response = await request(app).post('/api/login').send(testUser)
+            expect(response.statusCode).toBe(200)
+        }  catch (err) {
+    
+            console.log('Error')
+        }
+      });
