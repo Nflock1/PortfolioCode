b145ce8f97cdc0993e604c41d3ae006df3410942
nick <nflock@wisc.edu>
2021-12-08 19:23:20 -0600

    fixed the committing weirdness and app now posts properly

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const routes = require("./routes/user")
 
-function createServer(){
+require("dotenv").config({ path: "./config.env" });
+const mongoose = require("mongoose")
+const createServer = require("./server")
+const dbURL = "mongodb+srv://dcphillips99:00805061Dp!@ratemyrestroom.tobsj.mongodb.net/RateMyRestroom?retryWrites=true&w=majority";
+const port = process.env.PORT || 5000;
+ 
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded());
   app.use(routes);
-  return app
-}
-
-module.exports = createServer;
+ 
+  mongoose
+	.connect(dbURL, { useNewUrlParser: true })
+	.then(() => {
+		app.listen(port, () => {
+			console.log(`Server has started on port: ${port}`)
+		})
+	})
+module.exports = app;
