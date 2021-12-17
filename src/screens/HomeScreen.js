6ab242601429fdab45d5a9ed02842f3c064aa172
dcphillips99 <dcphillips@wisc.edu>
2021-12-17 15:17:26 -0600

    Added current progress of Homescreen (map) and submission pages

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -32,8 +32,31 @@ const STYLES = StyleSheet.create({
 function HomeScreen({navigation}) {
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
-    const [mapRegion, setMapRegion] = useState(null);
-    const [restrooms, setRestrooms] = useState(null);
+    const [mapRegion, setMapRegion] = useState({
+        longitude: -90,
+        latitude: 0,
+        longitudeDelta: 180,
+        latitudeDelta: 90});
+    const [restrooms, setRestrooms] = useState([
+        {   
+            id: 1,
+            latlng: {
+                latitude: 43.068096178875685,
+                longitude:-89.4044798846011
+            },
+            title: "dons",
+            description: "ok",
+        },
+        {
+            latlng: 
+            {latitude: 43.0677235708863,
+            longitude:-89.41025657181031
+            },
+            title: "bar",
+            id: 2,
+            description: "bad",
+        }
+    ]);
     const {signOut} = React.useContext(AuthContext);
     useEffect(() => {
         (async () => {
@@ -50,38 +73,53 @@ function HomeScreen({navigation}) {
             latitude: location.coords.latitude,
             longitudeDelta: 0.0922,
             latitudeDelta: 0.0421
-        })
-        setLocation(location);
-
-        const params = {
-            longitude: location.coords.longitude,
-            latitude: location.coords.latitude,
-            radius: 5
-        }
-        function getRestrooms() {
-            axios.get("/api/near-RR", params)
-                
-                .then((result) => {
-                    Alert.alert('Do Something API CALLED');
-                })
-                .catch(err => {
-                    console.log("Could not load restrooms.")
-                })
-        }
+            })
+            const params = {
+                longitude: parseFloat(location.coords.longitude),
+                latitude: parseFloat(location.coords.latitude),
+                radius: 5
+            }
+            let newarr = [];
+            let result = await axios.get("/api/near-RR", {params});
+            for (var i = 0; i < Object.keys(result.data.data).length; ++i) {
+                                const restroom ={
+                                    latlng: {longitude: result.data.data[i].longitude,
+                                    latitude: result.data.data[i].latitude,},
+                                    longitudeDelta: 0.0922,
+                                    latitudeDelta: 0.0421
+                                };
+                                newarr[i] = restroom;
+                            }
+            setRestrooms(newarr);
+        
         })();
       }, []);
 
     return(
 
         <SafeAreaView style = {{justifyContent: 'center', alignItems:'center', flex: 1}}>
-            <MapView initialRegion= {mapRegion} style={STYLES.map}>
+            <MapView region= {mapRegion} style={STYLES.map}
+            loadingEnabled = {true}
+            loadingIndicatorColor="#666666"
+            loadingBackgroundColor="#eeeeee"
+            moveOnMarkerPress = {false}
+            showsUserLocation={true}
+            showsCompass={true}
+            showsPointsOfInterest = {false}
+            provider="google">
+                
+                {console.log("map region: " +JSON.stringify(mapRegion))}
                 <Marker coordinate={mapRegion}></Marker>  
+                {restrooms ? restrooms.map((restrooms, index) => {
+                    <Marker 
+                    key={index}
+                    coordinate={restrooms.latlng}
+                    title={restrooms.title}
+                    description={restrooms.description}>
 
-                {restrooms ? restrooms.map(restrooms => {
-                    <Marker coordinate={restrooms.longitude, restrooms.latitude}/>
-                }): null}                  
+                    </Marker>
+                }): console.log("Didn't work")}                
             </MapView>
-            
             <Text>Hello You Are On The HOMEPAGE!</Text>
             <Button title= "Logout" onPress={()=> signOut()}></Button>
             {/* <Button title= "Add Restroom" onPress={() => navigation.navigate('SubmissonScreen')}></Button> */}
@@ -91,6 +129,7 @@ function HomeScreen({navigation}) {
 
 
     );
+
 
 }
 
