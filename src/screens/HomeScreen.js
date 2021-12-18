cc701b5005b58b4d0756805595e988650c9c4084
dcphillips99 <dcphillips@wisc.edu>
2021-12-17 21:35:57 -0600

    Finished implementing the map, including seeing sourrounding rrs

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,8 @@ import MapView, {Marker} from 'react-native-maps';
 import {StyleSheet,View, Text, SafeAreaView, Button, Dimensions, Alert} from 'react-native';
 import { AuthContext } from '../context';
 import * as Location from 'expo-location';
-
+import RestroomMarker from './restroomMarker';
+import SplashScreen from './SplashScreen';
 const STYLES = StyleSheet.create({
     buttonSignIn: {
         backgroundColor: 'dodgerblue',
@@ -32,93 +33,65 @@ const STYLES = StyleSheet.create({
 function HomeScreen({navigation}) {
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
-    const [mapRegion, setMapRegion] = useState({
-        longitude: -90,
-        latitude: 0,
-        longitudeDelta: 180,
-        latitudeDelta: 90});
-    const [restrooms, setRestrooms] = useState([
-        {   
-            id: 1,
-            latlng: {
-                latitude: 43.068096178875685,
-                longitude:-89.4044798846011
-            },
-            title: "dons",
-            description: "ok",
-        },
-        {
-            latlng: 
-            {latitude: 43.0677235708863,
-            longitude:-89.41025657181031
-            },
-            title: "bar",
-            id: 2,
-            description: "bad",
-        }
-    ]);
-    const {signOut} = React.useContext(AuthContext);
-    useEffect(() => {
-        (async () => {
-          let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-            return;
-          }
-          
-          let location = await Location.getCurrentPositionAsync({});
-          
-          setMapRegion({
-            longitude: location.coords.longitude,
-            latitude: location.coords.latitude,
-            longitudeDelta: 0.0922,
-            latitudeDelta: 0.0421
-            })
-            const params = {
-                longitude: parseFloat(location.coords.longitude),
-                latitude: parseFloat(location.coords.latitude),
-                radius: 5
-            }
-            let newarr = [];
-            let result = await axios.get("/api/near-RR", {params});
-            for (var i = 0; i < Object.keys(result.data.data).length; ++i) {
-                                const restroom ={
-                                    latlng: {longitude: result.data.data[i].longitude,
-                                    latitude: result.data.data[i].latitude,},
-                                    longitudeDelta: 0.0922,
-                                    latitudeDelta: 0.0421
-                                };
-                                newarr[i] = restroom;
-                            }
-            setRestrooms(newarr);
-        
-        })();
-      }, []);
+    const [mapRegion, setMapRegion] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const [restrooms, setRestrooms] = useState([]);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
 
+      let location = await Location.getCurrentPositionAsync({});
+      setMapRegion({
+          longitude: location.coords.longitude,
+          latitude: location.coords.latitude,
+          longitudeDelta: 0.0922,
+          latitudeDelta:0.0421
+      })
+
+      const params = {
+        longitude: parseFloat(location.coords.longitude),
+        latitude: parseFloat(location.coords.latitude),
+        radius: 5
+        }
+        let newarr = [];
+        let result = await axios.get("/api/near-RR", {params});
+        for (var i = 0; i < Object.keys(result.data.data).length; ++i) {
+                        const restroom ={
+                            latlng: 
+                                    {
+                                    longitude: Number(result.data.data[i].longitude),
+                                    latitude: Number(result.data.data[i].latitude),
+                                    },
+                            longitudeDelta: 0.0922,
+                            latitudeDelta: 0.0421,
+                            title: result.data.data[i].name,
+                        };
+                        newarr[i] = restroom;
+                    }
+        setRestrooms(newarr);
+        setLoading(false);
+    })();
+  }, []);
+
+  if (isLoading) {
+    return (SplashScreen());
+  }
     return(
 
         <SafeAreaView style = {{justifyContent: 'center', alignItems:'center', flex: 1}}>
-            <MapView region= {mapRegion} style={STYLES.map}
-            loadingEnabled = {true}
-            loadingIndicatorColor="#666666"
-            loadingBackgroundColor="#eeeeee"
-            moveOnMarkerPress = {false}
-            showsUserLocation={true}
-            showsCompass={true}
-            showsPointsOfInterest = {false}
-            provider="google">
-                
-                {console.log("map region: " +JSON.stringify(mapRegion))}
-                <Marker coordinate={mapRegion}></Marker>  
-                {restrooms ? restrooms.map((restrooms, index) => {
-                    <Marker 
+            
+            <MapView style={STYLES.map} initialRegion={mapRegion} showsUserLocation={true} >
+                {restrooms ? restrooms.map((restrooms, index) => (
+                    <Marker
                     key={index}
                     coordinate={restrooms.latlng}
-                    title={restrooms.title}
-                    description={restrooms.description}>
-
-                    </Marker>
-                }): console.log("Didn't work")}                
+                    title = {restrooms.title}
+                    />
+                )): null}
             </MapView>
             <Text>Hello You Are On The HOMEPAGE!</Text>
             <Button title= "Logout" onPress={()=> signOut()}></Button>
