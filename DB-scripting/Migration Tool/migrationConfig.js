ceaee87686954703dbe8f2a230215e5e240a4d81
Nflock1 <nbw.kcin@gmail.com>
2024-06-26 11:31:18 -0500

    updated config and gitignore

--- DB-scripting/Migration Tool/migrationConfig.js
+++ DB-scripting/Migration Tool/migrationConfig.js
@@ -1,21 +1,23 @@
 module.exports = 
 { 
-  //this should be the URL to your MongoDB database
-  clusterURL: "",
-  databaseName: "",
-  collection: "",
+  //this should be the "connect" url"
+  clusterURL: "mongodb+srv://nflock:oKrIDrWV55SQef0A@cluster0.8jcsfe0.mongodb.net?retryWrites=true&w=majority&appName=Cluster0",
+  //the name will be inserted into above URL at runtime
+  databaseName: "sample_airbnb",
+  //the collection of objects that will be changed
+  collection: "listingsAndReviews",
   query: [ 
     {
-    key: "",
-    value: ""
+    key: "listing_url",
+    value: "https://www.airbnb.com/rooms/10006546"
     }
   ],
   updates: [
     {
-      oldName:"",
+      oldName:"price",
       oldValue:"",
       newName:"",
-      newValue: ""
+      newValue: 90.00
     }
   ]
 }
